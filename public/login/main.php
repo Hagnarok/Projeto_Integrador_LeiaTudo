a124b245b04f3d63d3e3419d086ec5ff97a2c2e6
Gustavo Henrique de Araujo Alves <61398911+Hagnarok@users.noreply.github.com>
2025-09-26 02:28:01 +0000

    css teste

--- public/login/main.php
+++ public/login/main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Login - LeiaTudo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/default.css" rel="stylesheet">
 
 </head>
 <body class="home-login">
